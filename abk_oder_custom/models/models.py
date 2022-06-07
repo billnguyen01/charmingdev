748ca4c6131e57d02f0e9c40db32d3228e50aba5
Peter Nguyen <haonguyenhcmus@gmail.com>
2022-06-07 17:08:02 +0700

    upload warning in delivery

--- abk_oder_custom/models/models.py
+++ abk_oder_custom/models/models.py
@@ -9,6 +9,18 @@ class custom_sale_order(models.Model):
     remaining = fields.Integer('Remaining')
     payment_schedule = fields.Selection([('late', 'Late'), ('not_late', 'Not late')], string="Payment Schedule")
     can_deliver = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Can deliver")
+    count_paid = fields.Many2many("account.move", string='Count Paid')
+
+
+class warning_delivery(models.Model):
+    _inherit = 'stock.picking'
+    delivery_warning = fields.Boolean('Delivery Warning', compute="compute_delivery_warning")
+
+    def compute_delivery_warning(self):
+        for record in self:
+            record['delivery_warning'] = record.sale_id.is_warning
+
+
 
 
 
