143e1269c96640c3f66fcb9a93ae7fd2ae2e4d33
Jacky <tdhungit@gmail.com>
2022-06-24 14:59:21 +0700

    subcontracting

---
+++ subcontracting_workorder_advance/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Subcontracting in Work Orders",
+
+    'summary': """
+        Subcontracting in Work Orders""",
+
+    'description': """
+        Subcontracting in Work Orders
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Customizations',
+    'version': '0.1',
+
+    'depends': ['base', 'mrp', 'purchase', 'sale', 'account', 'stock'],
+
+    'data': [
+        'views/component_product_bom.xml'
+    ],
+}
