f804ae727c7d3b82623357dd352d0314b13092e0
Jacky <tdhungit@gmail.com>
2022-06-27 13:56:25 +0700

    subcontracting

--- subcontracting_workorder_advance/models/mrp_workorder.py
+++ subcontracting_workorder_advance/models/mrp_workorder.py
@@ -10,5 +10,7 @@ _logger = logging.getLogger(__name__)
 class MrpWorkOrderSubcontract(models.Model):
     _inherit = 'mrp.workorder'
 
+    bill_of_material = fields.Many2one('mrp.bom', string='Bill Of Material')
+
     def button_subcontract(self):
         _logger.info('Click subcontract')
