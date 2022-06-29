ad6f52dde0fef33ef8c55126e68e089f6f86a7b6
Jacky <tdhungit@gmail.com>
2022-06-29 07:22:23 +0700

    subcontracting

--- subcontracting_workorder_advance/models/mrp_workorder.py
+++ subcontracting_workorder_advance/models/mrp_workorder.py
@@ -10,6 +10,13 @@ _logger = logging.getLogger(__name__)
 class MrpWorkOrderSubcontract(models.Model):
     _inherit = 'mrp.workorder'
 
+    is_subcontract = fields.Char(compute='_compute_is_subcontract', string="Subcontract?", store=False)
+
+    @api.depends('operation_id')
+    def _compute_is_subcontract(self):
+        for record in self:
+            record['is_subcontract'] = record.operation_id.is_subcontract
+
     def button_subcontract(self):
         self.create_po()
         self.create_transfer_go()
